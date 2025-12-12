1c8b09924ce6f3887a7d53cc985e36c9eb7eedad
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 09:06:54 -0500

    Update timeMonitor.h

--- timeMonitor.h
+++ timeMonitor.h
@@ -1,10 +1,3 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-04-10-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #ifndef SENECA_TIMEMONITOR_H
 #define SENECA_TIMEMONITOR_H
 
@@ -22,5 +15,6 @@ namespace seneca {
         Event stopEvent();
     };
 }
+
 
 #endif // SENECA_TIMEMONITOR_H
