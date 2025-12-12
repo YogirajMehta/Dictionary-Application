5d79723bfd4755c9412f48ec0fb219488aa4e93b
Yogiraj Mehta <yogirajmehta.yrm@gmail.com>
2025-12-12 09:07:36 -0500

    Update settings.cpp

--- settings.cpp
+++ settings.cpp
@@ -1,11 +1,5 @@
-/*
-Name-Yogiraj Mehta
-email-ymehta4@myseneca.ca
-id-162867238
-date-04-10-2025
-I declare that this submission is the result of my own work and I only copied the code that my professor provided to complete my assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
-*/
 #include "settings.h"
 namespace seneca {
 	settings g_settings;
+
 }
